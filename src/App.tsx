d3c0f4feb1a0fbb6f3eb144754384decc39bacd0
revengel <natalia200218@yandex.ru>
2021-11-27 02:26:20 +0400

    ready lab6

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Counter from './Counter';
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload!
         </p>
         <a
           className="App-link"
@@ -18,9 +19,9 @@ function App() {
         >
           Learn React
         </a>
+        <Counter start={10} step={5}/>
       </header>
     </div>
   );
 }
-
 export default App;
